c6c83f2fcc0b39c806953a45bcaa8b5cbfddd68b
GustafJ <gustaf89@hotmail.se>
2021-05-11 16:25:48 +0200

    fix modals

--- views/View.php
+++ views/View.php
@@ -6,12 +6,17 @@ class View
     include_once("views/partials/header.php");
   }
 
+  public function viewHero()
+  {
+    include_once("views/partials/hero.php");
+  }
+
   public function viewFooter()
   {
     include_once("views/partials/footer.php");
   }
 
-  public function viewRecordsStart()
+  public function viewRecordsStart($genres)
   {
     include_once("views/partials/recordsStart.php");
   }
@@ -42,10 +47,51 @@ class View
     echo $html;
   }
 
+  public function viewModal($record)
+  { 
+    $html = <<<HTML
+                  <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-labelledby="portfolioModal1Label" aria-hidden="true">
+                          <div class="modal-dialog modal-xl" role="document">
+                              <div class="modal-content">
+                                  <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                      <span aria-hidden="true"><i class="fas fa-times"></i></span>
+                                  </button>
+                                  <div class="modal-body text-center">
+                                      <div class="container">
+                                          <div class="row justify-content-center">
+                                              <div class="col-lg-8">
+                                                  <!-- Portfolio Modal - Title-->
+                                                  <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0" id="portfolioModal1Label">$record[title]</h2>
+                                                  <!-- Icon Divider-->
+                                                  <div class="divider-custom">
+                                                      <div class="divider-custom-line"></div>
+                                                      <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                                                      <div class="divider-custom-line"></div>
+                                                  </div>
+                                                  <!-- Portfolio Modal - Image-->
+                                                  <img class="img-fluid rounded mb-5" src="$record[cover]" alt="..." />
+                                                  <!-- Portfolio Modal - Text-->
+                                                  <p class="mb-5">$record[description]</p>
+                                                  <button class="btn btn-primary" data-dismiss="modal">
+                                                      <i class="fas fa-times fa-fw"></i>
+                                                      Close Window
+                                                  </button>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+        HTML;
+    echo $html;
+  }
+
   public function viewAllRecords($records)
   {
     foreach ($records as $record) {
       $this->viewOneRecord($record);
+      $this->viewModal($record);
     }
   }
 }
