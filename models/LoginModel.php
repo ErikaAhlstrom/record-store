5b38624e10dcb1ebd98f2845c875f36b48cc843c
Marta Pettersson <marta.pettersson@gmail.com>
2021-05-17 16:01:48 +0200

    Add basics to Login and Register Models

--- models/LoginModel.php
+++ models/LoginModel.php
@@ -1,4 +1,15 @@
 <?php
 class LoginModel
 {
+    private $db;
+
+    public function __construct($database)
+    {
+        $this->db = $database;
+    }
+
+    public function loginCustomer(/*kanskeparameterhär*/)
+    {
+        // Kolla om customer finns och returnera true or false 
+    }
 }
--- models/RegisterModel.php
+++ models/RegisterModel.php
@@ -1,4 +1,15 @@
 <?php
 class RegisterModel
 {
+    private $db;
+
+    public function __construct($database)
+    {
+        $this->db = $database;
+    }
+
+    public function registerCustomer(/*kanskeparameterhär*/)
+    {
+        // Kolla om customer finns annars skapa!
+    }
 }
