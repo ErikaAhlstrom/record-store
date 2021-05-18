db2a1f321546c5ca75b5a7892c1666a7466a8c8c
“erikaahlstrom” <info@erikasoldh.se>
2021-05-18 14:39:15 +0200

    work on cart page.

--- views/CartView.php
+++ views/CartView.php
@@ -20,64 +20,90 @@ class CartView
     {
         $url = URLROOT;
 
-        print_r($cart);
+        $html = <<<HTML
 
-        // $html = <<<HTML
+            <div class="col-md-6">
+                    <div class="card m-1">
+                        <img class="card-img-top" src="" 
+                             alt="">
+                        <div class="card-body">
+                            <div class="card-title text-center">
+                                <h4>Album Title</h4>
+                                <h5>Price: 14.99 $</h5>
+                            </div>
+                        </div>
+                    </div>
+            </div>  <!-- col -->
 
-        //     <div class="col-md-6">
-        //             <div class="card m-1">
-        //                 <img class="card-img-top" src="$url/images/$movie[image]" 
-        //                      alt="$movie[title]">
-        //                 <div class="card-body">
-        //                     <div class="card-title text-center">
-        //                         <h4>$movie[title]</h4>
-        //                         <h5>Pris: $movie[price] kr</h5>
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //     </div>  <!-- col -->
+        HTML;
 
-        // HTML;
-
-        // echo $html;
+        echo $html;
     }
 
 
 
     public function viewCartPage($cart)
     {
-        $this->viewOneCart($cart);
+        // Tomt form som triggar ett köp
+        // 
         $this->viewOrderForm($cart);
+        $this->viewCart($cart);
+        
     }
 
-
+    // Detta är det som läggs till i en order
     public function viewOrderForm($cart)
     {
 
-        // $html = <<<HTML
+        $html = <<<HTML
+                <div class="col-md-6">
+                <h2>Total</h2>
+                <form action="#" method="post">
 
-        //     <div class="col-md-6">
+                    <!--   order detail 1 -->
+                    <input  hidden
+                            name="record_id" 
+                            value="$cart[id_record]"
+                            class="form-control form-control-lg my-2">
+                    <!-- Hämta customer id från session ist -->
 
-        //         <form action="#" method="post">
-        //             <input type="hidden" name="cart_id" 
-        //                     value="$cart[id_cart]">
+                    <!-- Skicka löp form till annan sida? -->
+                    <input type="submit" 
+                            name="delete"
+                            class="form-control my-2 btn btn-lg btn-outline-success" 
+                            value="Send Order">
+                </form>
+                <form action="#" method="post">
 
-        //             <input type="number" name="customer_id" required 
-        //                     class="form-control form-control-lg my-2" 
-        //                     placeholder="Ange ditt kundnummer">
+                    <!--   order detail 2 -->
+                    <input  hidden
+                            name="record_id" 
+                            value="$cart[id_record]"
+                            class="form-control form-control-lg my-2">
 
-        //             <input type="submit" class="form-control my-2 btn btn-lg btn-outline-success" 
-        //                     value="Send Order">
-        //         </form>
+                    <!-- Skicka löp form till annan sida? -->
+                    <input type="submit" 
+                            name="delete"
+                            class="form-control my-2 btn btn-lg btn-outline-success" 
+                            value="Send Order">
+                </form>
+                </div>
 
-        //     <!-- col avslutas efter ett meddelande från viewConfirmMessage eller viewErrorMessage -->
+            <!-- col avslutas efter ett meddelande från viewConfirmMessage eller viewErrorMessage -->
 
-        // HTML;
+        HTML;
 
-        // echo $html;
+        echo $html;
     }
 
-    public function viewConfirmMessage($customer, $lastInsertId)
+    public function viewCart($carts) {
+        foreach ($carts as $cart) {
+            $this->viewOneCart($cart);
+            var_dump($cart);
+        }
+    }
+
+/*     public function viewConfirmMessage($customer, $lastInsertId)
     {
         $this->printMessage(
             "<h4>Tack $customer[name]</h4>
@@ -88,9 +114,9 @@ class CartView
             ",
             "success"
         );
-    }
+    } */
 
-    public function viewErrorMessage($customer_id)
+/*     public function viewErrorMessage($customer_id)
     {
         $this->printMessage(
             "<h4>Kundnummer $customer_id finns ej i vårt kundregister!</h4>
@@ -99,7 +125,7 @@ class CartView
             ",
             "warning"
         );
-    }
+    } */
 
     /**
      * En funktion som skriver ut ett felmeddelande
