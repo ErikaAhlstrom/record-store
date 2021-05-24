fbc26211da71d264ef651454f4d3f203c2c768d5
“erikaahlstrom” <info@erikasoldh.se>
2021-05-24 11:39:04 +0200

    Add comments to admin model.

--- models/AdminModel.php
+++ models/AdminModel.php
@@ -9,18 +9,104 @@ class AdminModel
         $this->db = $database;
     }
 
-    public function loginAdmin($username, $password)
+    /*******************************
+                CREATE
+    ********************************/
+
+    public function insertArtist($name)
     {
-        $statement = "SELECT * FROM admin WHERE username = :username";
+        $statement = "INSERT INTO artists (name) VALUES (:name)";
+        $parameters = array(':name' => $name);
+        return $this->db->insert($statement, $parameters);
+    }
 
-        $parameters = array(":username" => $username);
-        $admin = $this->db->select($statement, $parameters)[0];
+    public function insertRecordsHasArtists($artist_id, $record_id)
+    {
+        $statement = "INSERT INTO records_has_artists VALUES (:record_id, :artist_id)";
+        $parameters = array(
+            ':artist_id' => $artist_id,
+            ':record_id' => $record_id
+        );
+        $this->db->insert($statement, $parameters);
+    }
 
-        if (empty($admin) || !password_verify($password, $admin['password'])) {
-            throw new Exception("Email or password is wrong");
-        }
+    public function insertProduct($record)
+    {
+        $statement = "INSERT INTO records (title, description, price, year_released, cover, stock) VALUES (:title, :description, :price, :year_released, :cover, :stock)";
+        $parameters = array(
+            ":title" => $record['title'],
+            ":description" => $record['description'],
+            ":price" => $record['price'],
+            ":year_released" => $record['year_released'],
+            ":cover" => "assets/img/record_player.png",
+            ":stock" => $record['stock']
+        );
+        $lastInsertId = $this->db->insert($statement, $parameters);
+        return $lastInsertId;
+    }
+    /*******************************
+                READ
+     ********************************/
 
-        return $admin;
+    public function fetchAllOrders()
+    {
+        $orders = $this->db->select(
+            "SELECT o.id_order, o.sent, o.timestamp, CONCAT(c.firstname, ' ', c.lastname) AS name 
+            FROM orders o 
+            JOIN customers c 
+            ON o.id_customer = c.id_customer
+            ORDER BY o.sent ASC
+            "
+        );
+        return $orders;
+    }
+
+    public function fetchOrderById($id)
+    {
+        $statement = "SELECT orders.id_order, records.title, records.id_record, order_details.amount, orders.sent FROM orders 
+        JOIN order_details ON order_details.orders_id_order = orders.id_order 
+        JOIN records ON records.id_record = order_details.records_id_record 
+        WHERE orders.id_order = :id";
+        $parameters = array(":id" => $id);
+        $order = $this->db->select($statement, $parameters);
+        return $order;
+    }
+
+    public function fetchRecordsHasArtistsById($id_record)
+    {
+        $statement = "SELECT * FROM records_has_artists WHERE id_record = :id_record";
+        $parameters = array(":id_record" => $id_record);
+        $records_has_artists = $this->db->select($statement, $parameters);
+        return $records_has_artists;
+    }
+
+    public function fetchArtistByName($name)
+    {
+        $statement = "SELECT * FROM artists WHERE name = :name";
+        $parameters = array(':name' => $name);
+        $artist = $this->db->select($statement, $parameters);
+        return $artist;
+    }
+
+    public function fetchAllArtists()
+    {
+        $artists = $this->db->select(
+            "SELECT * FROM artists"
+        );
+        return $artists;
+    }
+
+    public function fetchRecordById($id)
+    {
+        $statement = "SELECT r.title, r.description, r.price, r.year_released, r.stock, a.id_artist, a.name FROM records r
+        JOIN records_has_artists rha
+        ON r.id_record=rha.id_record 
+        JOIN artists a
+        ON rha.id_artist=a.id_artist
+        WHERE r.id_record = :id";
+        $parameters = array(":id" => $id);
+        $record = $this->db->select($statement, $parameters);
+        return $record;
     }
 
     public function fetchAllRecords()
@@ -35,29 +121,19 @@ class AdminModel
         );
         return $records;
     }
+    /*******************************
+                UPDATE
+     ********************************/
 
-    public function fetchAllArtists()
+    public function updateRecordsHasArtists($id_record, $id_artist)
     {
-        $artists = $this->db->select(
-            "SELECT * FROM artists"
-        );
-        return $artists;
+        $statement = "UPDATE records_has_artists SET id_artist = :id_artist WHERE id_record = :id_record";
+        $parameters = array(":id_artist" => $id_artist, ":id_record" => $id_record);
+        $this->db->update($statement, $parameters);
     }
 
-    public function fetchRecordById($id) 
+    public function updateRecord($id, $record)
     {
-        $statement = "SELECT r.title, r.description, r.price, r.year_released, r.stock, a.id_artist, a.name FROM records r
-        JOIN records_has_artists rha
-        ON r.id_record=rha.id_record 
-        JOIN artists a
-        ON rha.id_artist=a.id_artist
-        WHERE r.id_record = :id";
-        $parameters = array(":id" => $id);
-        $record = $this->db->select($statement, $parameters);
-        return $record;
-    }
-
-    public function updateRecord($id, $record) {
         $statement = "UPDATE records
         SET title = :title, description = :description, price = :price, year_released = :year_released, stock = :stock 
         WHERE id_record = :id";
@@ -71,76 +147,43 @@ class AdminModel
         );
         $this->db->update($statement, $parameters);
     }
+    /*******************************
+                DELETE
+     ********************************/
 
-    public function insertProduct($record) {
-        $statement = "INSERT INTO records (title, description, price, year_released, cover, stock) VALUES (:title, :description, :price, :year_released, :cover, :stock)";
-        $parameters = array(
-            ":title" => $record['title'],
-            ":description" => $record['description'],
-            ":price" => $record['price'],
-            ":year_released" => $record['year_released'],
-            ":cover" => "assets/img/record_player.png",
-            ":stock" => $record['stock']
-        );
-        $lastInsertId = $this->db->insert($statement, $parameters);
-        return $lastInsertId;
-    }
 
-    public function updateRecordsHasArtists($id_record, $id_artist) {
-        $statement = "UPDATE records_has_artists SET id_artist = :id_artist WHERE id_record = :id_record";
-        $parameters = array(":id_artist" => $id_artist, ":id_record" => $id_record);
-        $this->db->update($statement, $parameters);
-    }
+    /*******************************
+            HELP METHODS
+     ********************************/
 
-    public function fetchRecordsHasArtistsById($id_record) {
-        $statement = "SELECT * FROM records_has_artists WHERE id_record = :id_record";
-        $parameters = array(":id_record" => $id_record);
-        $records_has_artists = $this->db->select($statement, $parameters);
-        return $records_has_artists;
-    }
-
-    public function fetchArtistByName($name) {
-        $statement = "SELECT * FROM artists WHERE name = :name";
-        $parameters = array(':name' => $name);
-        $artist = $this->db->select($statement, $parameters);
-        return $artist;
-    }
-
-    public function insertArtist($name) {
-        $statement = "INSERT INTO artists (name) VALUES (:name)";
-        $parameters = array(':name' => $name);
-        return $this->db->insert($statement, $parameters);
-    }
-
-    public function insertRecordsHasArtists($artist_id, $record_id) {
-        $statement = "INSERT INTO records_has_artists VALUES (:record_id, :artist_id)";
-        $parameters = array(':artist_id' => $artist_id,
-                            ':record_id' => $record_id);
-        $this->db->insert($statement, $parameters);
-    }
-
-    public function fetchAllOrders()
+    public function loginAdmin($username, $password)
     {
-        $orders = $this->db->select(
-            "SELECT o.id_order, o.sent, o.timestamp, CONCAT(c.firstname, ' ', c.lastname) AS name 
-            FROM orders o 
-            JOIN customers c 
-            ON o.id_customer = c.id_customer
-            ORDER BY o.sent ASC
-            ");
-        return $orders;
+        $statement = "SELECT * FROM admin WHERE username = :username";
+
+        $parameters = array(":username" => $username);
+        $admin = $this->db->select($statement, $parameters)[0];
+
+        if (empty($admin) || !password_verify($password, $admin['password'])) {
+            throw new Exception("Email or password is wrong");
+        }
+
+        return $admin;
     }
 
-    public function fetchOrderById($id)
-    {
-        $statement = "SELECT orders.id_order, records.title, records.id_record, order_details.amount, orders.sent FROM orders 
-        JOIN order_details ON order_details.orders_id_order = orders.id_order 
-        JOIN records ON records.id_record = order_details.records_id_record 
-        WHERE orders.id_order = :id";
-        $parameters = array(":id" => $id);
-        $order = $this->db->select($statement, $parameters);
-        return $order;
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public function setToSent($id) {
         $statement = "UPDATE orders SET sent = 1 WHERE id_order = :id";
